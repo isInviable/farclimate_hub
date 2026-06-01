643628f3b2dad43348985236d206df3750719345
Sergio Galán <sergio@eclectico.net>
2026-06-02 01:16:26 +0200

    Quitar favicon de nuxt Fixes #12

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -4,7 +4,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/png', href: '/img/icono-farclimate.png' }
   ],
   htmlAttrs: {
     lang: 'en'
